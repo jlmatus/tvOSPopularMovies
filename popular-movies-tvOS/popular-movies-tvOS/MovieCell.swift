88c0a366b6722bebbd866ecf9e90e2b2b7e78385
Jose Luis Matus <josem@mediarain.com.ni>
2016-01-04 10:11:17 -0600

    creating the initial layout.

---
+++ popular-movies-tvOS/popular-movies-tvOS/MovieCell.swift
@@ -0,0 +1,18 @@
+//
+//  MovieCell.swift
+//  popular-movies-tvOS
+//
+//  Created by Jose on 1/4/16.
+//  Copyright © 2016 Rain Agency. All rights reserved.
+//
+
+import UIKit
+
+class MovieCell: UICollectionViewCell {
+    @IBOutlet weak var movieImage: UIImageView!
+    @IBOutlet weak var movieTitel: UILabel!
+ 
+    func configureCell() {
+        
+    }
+}
